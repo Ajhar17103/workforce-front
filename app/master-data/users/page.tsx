72f5c470336777c3037ddb3cdadf632841d3b3ff
azhar <contact.ajharislam@gmail.com>
2025-08-10 18:54:38 +0600

    change structure

--- app/master-data/users/page.tsx
+++ app/master-data/users/page.tsx
@@ -1,13 +1,4 @@
 'use client';
-
 import UserPage from "@/component/users/UserPage";
 
-
-export default function Users() {
-  
-  return (
-    <div className="g-5">
-      <UserPage />
-    </div>
-  );
-}
+export default function Users() {return<UserPage />}
